a2a63aa5da522ad1cb12cc9c579735f80172be08
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2026-06-05 15:37:07 +0600

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/com/tse/erp/TseErpApplication.java
@@ -0,0 +1,13 @@
+package com.tse.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TseErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TseErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tse/erp/TseErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tse.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TseErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
